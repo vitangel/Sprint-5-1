e8b784c9f296cd4ecdf301d24bea2ee1da960728
KAV <username@yandex.ru>
2026-02-06 22:45:12 +0300

    second commit

---
+++ src/main/java/com/example/Lion.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.util.List;
+
+public class Lion {
+    private final boolean hasMane;
+    private final Feline feline;  // Используем Feline напрямую для getKittens()
+
+    public Lion(String sex, Feline feline) throws Exception {
+        if ("Самец".equals(sex)) {
+            this.hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            this.hasMane = false;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+        }
+        this.feline = feline;
+    }
+
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.eatMeat();
+    }
+}
